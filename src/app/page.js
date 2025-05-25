d7daa0cc30b733929fadae3ae053e51639d30053
sairaj0204 <sairajpatil0204>
2025-05-25 14:22:57 +0530

    Just the Essentials

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,98 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
+function page() {
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    setMainTask([...mainTask, {value, desc}])
+    setDesc("")
+    setValue("")
+  }
+  const [value, setValue] = useState("")
+  const [desc, setDesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
+  const [completedTask, setCompletedTask] = useState([])
+
+  const deleteHandler = (i) =>{
+    let copyArray = [...mainTask]
+    copyArray.splice(i,1)
+    setMainTask(copyArray)
+  }
+
+  const completeHandler = (i) =>{
+      const taskToComplete = mainTask[i];
+      setCompletedTask([...completedTask, taskToComplete]);
+      mainTask.splice(i,1)
+  }
+  let renderCompleted = 'No task Completed'
+  if(completedTask.length>0){
+    renderCompleted = completedTask.map((c,i)=>{
+      return(
+        <li key={i}>
+          <h3>{c.value}</h3>
+          <h4>{c.desc}</h4>
+        </li>
+      )
+    })
+  }
+  let renderTask = 'No tasks Avaliable'
+  if (mainTask.length > 0){
+    renderTask = mainTask.map((t,i)=>{
+      return(
+        <li key={i}>
+          <div className='flex justify-between'>
+            <h5 className='text-3xl p-5'>{t.value}</h5>
+            <h6 className='text-2xl p-5 pr-5'>{t.desc}</h6>
+          </div>
+          <button onClick={()=>{
+            deleteHandler(i)
+          }}>
+            Delete
+          </button>
+          <button onClick={()=>{
+            completeHandler(i)
+          }}>
+            Complete
+          </button>
+        </li>
+      );
+    });
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <>
+    <h1>My Todo List</h1>
+    <form onSubmit={submitHandler} className='flex-row justify-items-center'>
+      <input className='bg-amber-50 m-5 p-5'
+        placeholder='Enter Task'
+        value={value}
+        onChange={(e)=>
+        {
+          setValue(e.target.value)
+        }
+        }
+        
+      />
+      <input
+        placeholder='Add Description'
+        className='bg-amber-100 p-10 m-5'
+        value={desc}
+        onChange={(e)=>
+          {
+          setDesc(e.target.value)
+          }
+        }
+      />
+      <button className='bg-green-600 p-5 rounded border-2'>Submit</button>
+    </form>
+    <div>
+      <ul>{renderTask}</ul>
     </div>
-  );
+    <hr></hr>
+    <div>
+      <ul>{renderCompleted}</ul>
+    </div>
+    </>
+  )
 }
+
+export default page
